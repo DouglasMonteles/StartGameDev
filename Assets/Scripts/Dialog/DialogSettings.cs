0dfd06eca37eb26a911c4c669c5416c5f7f74c90
DouglasMonteles <silvadouglas328@gmail.com>
2025-03-25 01:10:12 -0300

    :sparkles: add dialogue list config in the Unity Editor

--- Assets/Scripts/Dialog/DialogSettings.cs
+++ Assets/Scripts/Dialog/DialogSettings.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using System.Security.Cryptography;
 
 [CreateAssetMenu(fileName = "New Dialogue", menuName = "New Dialogue/Dialogue")]
 public class DialogSettings : ScriptableObject
@@ -37,3 +39,38 @@ public class Language {
     public string spanish;
 
 }
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(DialogSettings))]
+public class BuilderEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        DialogSettings dialogSettings = (DialogSettings) target;
+
+        Language languageSettings = new Language();
+        languageSettings.portuguese = dialogSettings.sentence;
+
+        Sentence sentenceSettings = new Sentence();
+        sentenceSettings.profile = dialogSettings.speakerSprite;
+        sentenceSettings.sentence = languageSettings;
+
+        if (GUILayout.Button("Create Dialogue"))
+        {
+            if (dialogSettings.sentence != "")
+            {
+                dialogSettings.dialogues.Add(sentenceSettings);
+
+                // clear fields
+                dialogSettings.speakerSprite = null;
+                dialogSettings.sentence = "";
+            }
+        }
+    }
+
+}
+
+#endif
