e8558cf0324988f43161fdc5b1a86235924d05d2
DouglasMonteles <silvadouglas328@gmail.com>
2025-04-20 16:20:52 -0300

    :sparkles: added enemy attack collision

--- Assets/Scripts/Enemy/AnimationControl.cs
+++ Assets/Scripts/Enemy/AnimationControl.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class AnimationControl : MonoBehaviour
 {
 
+    [SerializeField]
+    private Transform attackPoint;
+
+    [SerializeField]
+    private float radius;
+
+    [SerializeField]
+    private LayerMask playerLayer;
+
     private Animator animator;
 
     void Awake()
@@ -26,4 +35,24 @@ public class AnimationControl : MonoBehaviour
     {
         animator.SetInteger("transition", value);
     }
+
+    public void Attack()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, radius, playerLayer);
+
+        if (hit != null)
+        {
+            Debug.Log("attack");
+        }
+        else
+        {
+
+        }
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, radius);   
+    }
+
 }
