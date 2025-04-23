77eecf242be6c7d7f425795ab2d26ef41ba0127c
DouglasMonteles <silvadouglas328@gmail.com>
2025-04-23 00:28:10 -0300

    :sparkles: detecting when player is approaching to enemy

--- Assets/Scripts/Enemy/Skeleton.cs
+++ Assets/Scripts/Enemy/Skeleton.cs
@@ -14,6 +14,10 @@ public class Skeleton : MonoBehaviour
     public float totalHealth;
 
     public bool isDead;
+
+    public float radius;
+
+    public LayerMask playerLayer;
     
     [Header("Components")]
     [SerializeField]
@@ -23,6 +27,7 @@ public class Skeleton : MonoBehaviour
     private AnimationControl animationControl;
 
     private Player player;
+    private bool detectingPlayer;
 
 
     void Awake()
@@ -41,11 +46,12 @@ public class Skeleton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isDead)
+        if (isDead || !detectingPlayer)
         {
             return;
         }
         
+        agent.isStopped = false;
         agent.SetDestination(player.transform.position);
 
         if (Vector2.Distance(transform.position, player.transform.position) <= agent.stoppingDistance)
@@ -68,4 +74,31 @@ public class Skeleton : MonoBehaviour
             transform.eulerAngles = new Vector2(0, 180);
         }
     }
+
+    void FixedUpdate()
+    {
+        DetectPlayer();
+    }
+
+    public void DetectPlayer()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, radius, playerLayer);
+
+        if (hit != null)
+        {
+            detectingPlayer = true;
+        }
+        else
+        {
+            detectingPlayer = false;
+            animationControl.PlayAnimation(0);
+            agent.isStopped = true;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
 }
